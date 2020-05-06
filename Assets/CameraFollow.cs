54b5ed5bdbe21028b6d3bc66788a4b50268a77f4
CruelBoy17 <63223990+CruelBoy17@users.noreply.github.com>
2020-05-06 19:22:50 +0530

    Assests

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+	public Transform Player;
+	public float SmoothSpeed = 120f;
+	public Vector3 offset;
+
+	// Use this for initialization
+	void LateUpdate () {
+		transform.position = Player.position + offset;
+	}
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+	#region Variables
+	[Header("Transform")]
+    public Rigidbody rb;
+	public float ForwardForce = 500f;
+    public float SideForce = 400f;
+	public float JumpForce = 400f;
+	#endregion
+
+	// Use this for initialization
+	void Start () {
+		rb.AddForce(ForwardForce, 0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            rb.AddForce(0, 0, SideForce);
+        }
+        if (Input.GetKeyDown(KeyCode.D)) 
+        {
+            rb.AddForce(0, 0, -SideForce);
+        }
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			rb.AddForce(0, JumpForce, 0);
+		}
+	}
+}
